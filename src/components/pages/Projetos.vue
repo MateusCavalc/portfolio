8e231bb01fa3afca29492d3207cdd89bc2f9e222
MateusCavalc <mat.fcavalcanti@gmail.com>
2024-02-27 16:00:53 -0300

    Add Project Card to Project Page

--- src/components/pages/Projetos.vue
+++ src/components/pages/Projetos.vue
@@ -10,11 +10,84 @@
         <div class="project-card">
           <header class="card-title">
             <h4>Portfolio Web</h4>
+            <a href="https://github.com/MateusCavalc/portfolio"
+              ><font-awesome-icon icon="arrow-up-right-from-square"
+            /></a>
           </header>
           <hr />
           <p>
             Este portfolio foi desenvolvido como um projeto cujo objetivo é
             aplicar conceitos de FrontEnd.
+          </p>
+        </div>
+        <div class="project-card">
+          <header class="card-title">
+            <h4>Base de Conhecimento</h4>
+            <a href=""
+              ><font-awesome-icon icon="arrow-up-right-from-square"
+            /></a>
+          </header>
+          <hr />
+          <p>
+            Projeto destinado ao desenvolvimento de uma aplicação web completa,
+            integrada a base de dados relacionais e não-relacionais via API, e
+            com implementação de autenticação de acesso.
+          </p>
+        </div>
+        <div class="project-card">
+          <header class="card-title">
+            <h4>Outro Projeto</h4>
+            <a href=""
+              ><font-awesome-icon icon="arrow-up-right-from-square"
+            /></a>
+          </header>
+          <hr />
+          <p>
+            Projeto destinado ao desenvolvimento de uma aplicação web completa,
+            integrada a base de dados relacionais e não-relacionais via API, e
+            com implementação de autenticação de acesso.
+          </p>
+        </div>
+        <div class="project-card">
+          <header class="card-title">
+            <h4>Outro Projeto</h4>
+            <a href=""
+              ><font-awesome-icon icon="arrow-up-right-from-square"
+            /></a>
+          </header>
+          <hr />
+          <p>
+            Projeto destinado ao desenvolvimento de uma aplicação web completa,
+            integrada a base de dados relacionais e não-relacionais via API, e
+            com implementação de autenticação de acesso.
+          </p>
+        </div>
+        <div class="project-card">
+          <header class="card-title">
+            <h4>Mais Um</h4>
+            <a href=""
+              ><font-awesome-icon icon="arrow-up-right-from-square"
+            /></a>
+          </header>
+          <hr />
+          <p>
+            Projeto destinado ao desenvolvimento de uma aplicação web completa,
+            integrada a base de dados relacionais e não-relacionais via API, e
+            com implementação de autenticação de acesso.
+          </p>
+        </div>
+        <div class="project-card">
+          <header class="card-title">
+            <h4>E Outro</h4>
+            <a href=""
+              ><font-awesome-icon icon="arrow-up-right-from-square"
+            /></a>
+          </header>
+          <hr />
+          <p>
+            Projeto destinado ao desenvolvimento de uma aplicação web completa,
+            integrada a base de dados relacionais e não-relacionais via API, e
+            com implementação de autenticação de acesso.
           </p>
         </div>
       </section>
@@ -51,16 +124,34 @@ article {
 .projects section {
   display: flex;
   justify-content: space-around;
+  flex-wrap: wrap;
 }
 
 .projects .project-card {
-  width: 30%;
+  max-width: 30%;
+  height: fit-content;
   background-color: #0001;
   padding: 10px;
-  margin: 20px;
+  margin: 10px 5px;
 
   border-radius: 8px;
   box-shadow: rgba(0, 0, 0, 0.75) 3px 3px 10px 0px;
+}
+
+.projects .project-card .card-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.projects .project-card .card-title a {
+  margin-right: 5px;
+  text-decoration: none;
+  color: #000;
+}
+
+.projects .project-card svg {
+  height: 20px;
 }
 
 .projects .project-card hr {
@@ -68,7 +159,7 @@ article {
 }
 
 .projects .project-card p {
+  margin: 0;
   padding: 5px;
-  text-align: justify;
 }
 </style>
