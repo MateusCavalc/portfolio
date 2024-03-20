938961115e3dbc389750b74c99a40f800d3a4f5c
MateusCavalc <mat.fcavalcanti@gmail.com>
2024-03-20 17:40:56 -0300

    refactor menu items

--- src/components/template/Menu.vue
+++ src/components/template/Menu.vue
@@ -1,38 +1,53 @@
 <template>
   <aside class="menu">
-    <ul>
-      <li>
-        <router-link :to="{ name: 'home' }">
-          <a>Sobre Mim</a>
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'exp' }">
-          <a>Experiências</a>
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'comp' }">
-          <a>Competências</a>
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'certs' }">
-          <a>Cursos e Certificados</a>
-        </router-link>
-      </li>
-      <li>
-        <router-link :to="{ name: 'projects' }">
-          <a>Projetos</a>
-        </router-link>
-      </li>
-    </ul>
+    <nav class="h-100 w-100 p-2 d-flex flex-column">
+
+      <router-link :to="{ name: 'home' }">
+        <menu-item :active="activeItem === 'aboutme' || hoverItem == 'aboutme'" @click="activeItem = 'aboutme'"
+          @mouseover="hoverItem = 'aboutme'" @mouseleave="hoverItem = ''" label="Sobre mim"
+          icon="fa-regular fa-user"></menu-item>
+      </router-link>
+
+      <router-link :to="{ name: 'exp' }">
+        <menu-item :active="activeItem === 'exp' || hoverItem == 'exp'" @click="activeItem = 'exp'"
+          @mouseover="hoverItem = 'exp'" @mouseleave="hoverItem = ''" label="Experiências"
+          icon="fa-regular fa-compass"></menu-item>
+      </router-link>
+
+      <router-link :to="{ name: 'comp' }">
+        <menu-item :active="activeItem === 'comp' || hoverItem == 'comp'" @click="activeItem = 'comp'"
+          @mouseover="hoverItem = 'comp'" @mouseleave="hoverItem = ''" label="Competências"
+          icon="fa-solid fa-check-circle"></menu-item>
+      </router-link>
+
+      <router-link :to="{ name: 'certs' }">
+        <menu-item :active="activeItem === 'certs' || hoverItem == 'certs'" @click="activeItem = 'certs'"
+          @mouseover="hoverItem = 'certs'" @mouseleave="hoverItem = ''" label="Certificados"
+          icon="fa-solid fa-certificate"></menu-item>
+      </router-link>
+
+      <router-link :to="{ name: 'projects' }">
+        <menu-item :active="activeItem === 'projects' || hoverItem == 'projects'" @click="activeItem = 'projects'"
+          @mouseover="hoverItem = 'projects'" @mouseleave="hoverItem = ''" label="Projetos"
+          icon="fa-solid fa-laptop-code"></menu-item>
+      </router-link>
+
+    </nav>
   </aside>
 </template>
 
 <script>
+import MenuItem from './MenuItem.vue'
+
 export default {
   name: "portMenu",
+  components: { MenuItem },
+  data() {
+    return {
+      activeItem: 'aboutme',
+      hoverItem: 'aboutme',
+    }
+  },
 };
 </script>
 
@@ -40,40 +55,10 @@ export default {
 .menu {
   grid-area: menu;
   background: linear-gradient(to right, #232526, #414345);
-
-  display: flex;
-  justify-content: flex-start;
-
-  color: #ffffffb6;
-
   border-right: 1px solid #fff;
-}
-
-.menu ul {
-  list-style-type: none;
-  justify-self: flex-start;
-
-  padding: 10px 20px;
-
-  font-size: 1.2em;
 }
 
 .menu a {
   text-decoration: none;
-
-  color: #ffffffb6;
-}
-
-.menu li {
-  padding: 10px;
-}
-
-.menu li:hover {
-  background: linear-gradient(
-    90deg,
-    rgba(0, 0, 0, 0.25) 10%,
-    rgba(0, 0, 0, 0) 95%
-  );
-  border-radius: 8px;
 }
 </style>
