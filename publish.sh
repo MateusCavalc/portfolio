bdb5876fea7a1129dad4f7ca56c776d074a7275a
MateusCavalc <mat.fcavalcanti@gmail.com>
2024-03-20 22:37:41 -0300

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # get commit message
-read -p "> Commit message: " commit_msg
+# read -p "> Commit message: " commit_msg
 # echo $commit_msg
 
 # git stuff in main
-git add .
-git commit -am "$commit_msg" || { echo 'commit main failed' ; exit 1; }
-git push -u origin main || { echo 'push main failed' ; exit 1; }
+# git add .
+# git commit -am "$commit_msg" || { echo 'commit main failed' ; exit 1; }
+# git push -u origin main || { echo 'push main failed' ; exit 1; }
 
 # build application
-npm run build || { echo 'build failed' ; exit 1; }
+# npm run build || { echo 'build failed' ; exit 1; }
 
 # checkout to gh-pages
 git checkout gh-pages
@@ -24,9 +24,10 @@ cp ./dist/favicon.ico ./dist/index.html ./ || { echo 'cp files failed' ; exit 1;
 cp -r ./dist/css ./dist/img ./dist/js ./ || { echo 'cp folders failed' ; exit 1; }
 
 # publish in gh-pages
-git add .
-git commit -am $commit_msg || { echo 'commit gh-pages failed' ; exit 1; }
-git push -u origin gh-pages || { echo 'push gh-pages failed' ; exit 1; }
+# git add .
+# git commit -am "$commit_msg" || { echo 'commit gh-pages failed' ; exit 1; }
+# git push -u origin gh-pages || { echo 'push gh-pages failed' ; exit 1; }
+git status
 
 # go back to main
 git checkout main
