a843b56b3cde3dfc0c08d5c5edc491bb12399134
MateusCavalc <mat.fcavalcanti@gmail.com>
2024-03-25 23:37:46 -0300

    translate small cards to component

--- src/components/pages/Projetos/ProjetoCard.vue
+++ src/components/pages/Projetos/ProjetoCard.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>
-
+    <div :class="`project-card${small ? '-small' : ''}`">
+        <header class="card-title">
+            <h4>{{ title }}</h4>
+            <a :href="repoLink"><font-awesome-icon icon="arrow-up-right-from-square" /></a>
+        </header>
+        <p>
+            {{ description }}
+        </p>
+        <div class="proj-icons">
+            <slot>
+                [Icons slot]
+            </slot>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    name: "projetoCard"
+    name: "projetoCard",
+    props: {
+        small: Boolean,
+        title: String,
+        description: String,
+        repoLink: String
+    }
 }
 </script>
 <style></style>
